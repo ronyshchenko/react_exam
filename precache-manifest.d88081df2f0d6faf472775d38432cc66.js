fdd260270b3fd0048db588a9041cd0d71f428dc5
ronyshchenko <ronyshchenko@gmail.com>
2019-06-24 16:53:28 +0300

    Updates

---
+++ precache-manifest.d88081df2f0d6faf472775d38432cc66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3f4c74c5cd668914b30aadb7a3d6d1",
+    "url": "/react-exam/index.html"
+  },
+  {
+    "revision": "0ca6fcc33b9daecb9baf",
+    "url": "/react-exam/static/css/main.2f32d235.chunk.css"
+  },
+  {
+    "revision": "44a9230e9052e5999c73",
+    "url": "/react-exam/static/js/2.ccb108a6.chunk.js"
+  },
+  {
+    "revision": "0ca6fcc33b9daecb9baf",
+    "url": "/react-exam/static/js/main.a698b723.chunk.js"
+  },
+  {
+    "revision": "f71dafe36ebc70d5b2a4",
+    "url": "/react-exam/static/js/runtime~main.aec471ee.js"
+  }
+]);
